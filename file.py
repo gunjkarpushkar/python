0617551c0e9b4a0a6407371fbac7618330ab74c3
gunjkarpushkar <139943740+gunjkarpushkar@users.noreply.github.com>
2023-11-03 02:05:37 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,8 @@
+# Sample list of numbers
+numbers = [5, 2, 9, 1, 5, 6]
+
+# Perform a reverse sort
+sorted_numbers = sorted(numbers, reverse=True)
+
+# Print the sorted list in reverse order
+print(sorted_numbers)
